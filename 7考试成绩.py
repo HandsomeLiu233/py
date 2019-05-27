e4dfd49cd07756b5717c969bf147ee8076ad1a76
liuao <liuaoao66@outlook.com>
2019-05-27 08:44:56 +0800

    除了生成成绩的子函数生成的list无法调用之外，其余功能均正常

--- 7考试成绩.py
+++ 7考试成绩.py
@@ -1,24 +1,47 @@
 from random import randint
 
-#学生成绩生成
+# 学生成绩生成
+from typing import List, Any, Union
+
+
 def score():
-    lst1=[randint(1,100) for i in range(100)]
+    print('student score function is called')
+    lst1: List[Union[int, Any]] = [randint(1, 100) for i in range(100)]
     print(lst1)
 
-#冒泡排序，部分借鉴了书本
+
+
+# 冒泡排序，部分借鉴了书本
 def bubble(lst):
-    length=len(lst)
-    for i in range(0,length):
-        flag=False
-        for j in range(0,length-i-1):
-            judge=lst[j]<lst[j+1] #如果前小于后
+    length = len(lst)
+    for i in range(0, length):
+        flag = False
+        for j in range(0, length - i - 1):
+            judge = lst[j] < lst[j + 1]  # 如果前小于后
             if judge:
-                lst[j],lst[j+1]=lst[j+1],lst[j] #交换顺序
-                flag=True
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]  # 交换顺序
+                flag = True
         if not flag:
             break
 
-score()
-lst1=[randint(1,100) for i in range(100)]
-bubble(lst1)
-print(lst1)
+def grade(lst):
+    convert=[]
+    length=len(lst)
+    for k in range(0,length):
+        if lst[k]<60:
+            convert.append(str(lst[k])+'分 不及格')
+        elif lst[k]<=69:
+            convert.append(str(lst[k])+'分 及格')
+        elif lst[k]<=79:
+            convert.append(str(lst[k])+'分 一般')
+        elif lst[k]<=89:
+            convert.append(str(lst[k])+'分 良好')
+        else:
+            convert.append(str(lst[k])+'分 优秀')
+    print(convert)
+
+
+lst3: List[Union[int, Any]] = [randint(1, 100) for eee in range(100)] #生成随机成绩
+bubble(lst3) #排序
+print(lst3)
+grade(lst3)
